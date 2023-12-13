0d721bb48d44dc851cb305101392baca716a5ef2
malibu97 <alexander.staay@tu-dortmund.de>
2023-12-13 14:25:51 +0100

    minor clean up

--- creator_scripts/create_detection_etpu.py
+++
@@ -1,8 +0,0 @@
-import fiftyone
-dataset = fiftyone.zoo.load_zoo_dataset(
-    "coco-2017",
-    split="validation",
-    label_types=["detections", "segmentations"],
-    classes=["person", "car"],
-    max_samples=50,
-)
