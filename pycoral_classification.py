6dcf91ffe4f8aa714ca26821421bdb2b4a98076d
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2023-12-16 00:38:47 +0100

    (re)fixed batch size

--- pycoral_classification.py
+++ pycoral_classification.py
@@ -141,7 +141,7 @@ def tf_inference(model_name,x,targetDir):
   x_to_predict = x.squeeze()
   emissions_tracker.start()
   tflite_start_time = time.time()
-  prediction = model.predict(x_to_predict, batch_size=16)
+  prediction = model.predict(x_to_predict, batch_size=64)
   tflite_end_time = time.time()
   emissions_tracker.stop()
   final_predictions_1 = []
@@ -273,7 +273,7 @@ if __name__ == '__main__':
                       'accuracy_k5': accuracy_k5,
                       'accuracy_k10': accuracy_k10,
                       'validation_size': imageCount,
-                      'batch_size': 16 if backend == 'tf_gpu' or backend == 'tf_cpu' else 1,
+                      'batch_size': 64 if backend == 'tf_gpu' or backend == 'tf_cpu' else 1,
                       'task': 'classification'
                   }
     print(results)
